111bfc85fc2413b0775ce1e612002e1a52af2841
seva <seva_voloshin@mail.ru>
2018-06-22 17:33:20 +0300

    got clients from json, deleted unused files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import clientsData from './clients.json';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container p-5">
+        <div className="row">
+          <div className="col-5">
+            {
+              clientsData.map(function(person){
+                return (
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="media">
+                        <img src={person.general.avatar} style={{width: '100px', height: '100px'}}/>
+                        <div className="media-body pl-3">
+                          <b>{person.general.firstName} {person.general.lastName}</b>
+                          <p>{person.job.company}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            }
+          </div>
+        </div>
       </div>
     );
   }
