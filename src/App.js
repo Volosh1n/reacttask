b02596a8eee04fae766e35a1034e2c79c991eb84
seva <seva_voloshin@mail.ru>
2018-06-23 15:43:40 +0300

    created component media, added inside cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import clientsData from './clients.json';
 
+function Media(props) {
+  return (
+    <div className="media">
+      <img src={props.person.general.avatar} style={{width: '88px', height: '88px'}}/>
+      <div className="media-body pl-3">
+        <b>{props.person.general.firstName} {props.person.general.lastName}</b>
+        <p>{props.person.job.company}</p>
+      </div>
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
       <div className="container p-5">
         <div className="row">
-          <div className="col-5">
+          <div className="col-4">
             {
               clientsData.map(function(person){
                 return (
                   <div className="card">
                     <div className="card-body">
-                      <div className="media">
-                        <img src={person.general.avatar} style={{width: '100px', height: '100px'}}/>
-                        <div className="media-body pl-3">
-                          <b>{person.general.firstName} {person.general.lastName}</b>
-                          <p>{person.job.company}</p>
-                        </div>
-                      </div>
+                      <Media person={person} />
                     </div>
                   </div>
                 );
