b6bb9f8b76d5299f573675cb76320ea841fb448b
seva <seva_voloshin@mail.ru>
2018-06-23 16:44:32 +0300

    created component Card, minor improvements

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import clientsData from './clients.json';
 function Media(props) {
   return (
     <div className="media">
-      <img src={props.person.general.avatar} style={{width: '88px', height: '88px'}}/>
+      <img src={props.person.general.avatar} alt="avatar" style={{width: 88}}/>
       <div className="media-body pl-3">
         <b>{props.person.general.firstName} {props.person.general.lastName}</b>
         <p>{props.person.job.company}</p>
@@ -13,6 +13,16 @@ function Media(props) {
   );
 }
 
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <Media person={props.person} />
+      </div>
+    </div>
+  );
+}  
+
 class App extends Component {
   render() {
     return (
@@ -20,16 +30,15 @@ class App extends Component {
         <div className="row">
           <div className="col-4">
             {
-              clientsData.map(function(person){
+              clientsData.map(function(person, index){
                 return (
-                  <div className="card">
-                    <div className="card-body">
-                      <Media person={person} />
-                    </div>
-                  </div>
+                  <Card person={person} />
                 );
               })
             }
+          </div>
+          <div className="col-8 p-0">
+            {/* <Profile person={person} /> TODO: LOAD AFTER CLICK */}
           </div>
         </div>
       </div>
