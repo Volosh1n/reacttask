9500d57668de2d4ad8541bbbc7c0c0cbdfebe972
seva <seva_voloshin@mail.ru>
2018-06-25 19:47:50 +0300

    added onClick event to cards; minor improvements in App class

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 import React, { Component } from 'react';
 import clientsData from './clients.json';
 
-function Media(props) {
-  return (
-    <div className="media">
-      <img src={props.person.general.avatar} alt="avatar" style={{width: 88}}/>
-      <div className="media-body pl-3">
-        <b>{props.person.general.firstName} {props.person.general.lastName}</b>
-        <p>{props.person.job.company}</p>
-      </div>
-    </div>
+var Card = function({person, onClick}) {
+  return(
+    clientsData.map((person, index) => {
+      return (
+        <div className="card" key={index} onClick={() => onClick(person)}>
+          <div className="card-body">
+            <div className="media">
+              <img src={person.general.avatar} alt="avatar" style={{width: 88}}/>
+              <div className="media-body pl-3">
+                <b>{person.general.firstName} {person.general.lastName}</b>
+                <p>{person.job.company}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    })
   );
 }
 
-function Card(props) {
-  return (
-    <div className="card">
-      <div className="card-body">
-        <Media person={props.person} />
-      </div>
-    </div>
-  );
-}  
-
 class App extends Component {
+  state = { currentClient: false };
+
+  handleClick = currentClient => {
+    this.setState({ currentClient })
+    console.log(currentClient); // temp
+  };
+
   render() {
     return (
       <div className="container p-5">
         <div className="row">
           <div className="col-4">
-            {
-              clientsData.map(function(person, index){
-                return (
-                  <Card person={person} />
-                );
-              })
-            }
+            <Card onClick={this.handleClick} />
           </div>
           <div className="col-8 p-0">
             {/* <Profile person={person} /> TODO: LOAD AFTER CLICK */}
